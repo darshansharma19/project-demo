17bd91fb75dc471371b1885d7c4beb6fd1b39f1c
Abhinav <abhinavtomar251@gmail.com>
2023-12-27 01:13:27 +0530

    changes in header components

--- Frontend/app/components/Header/page.tsx
+++ Frontend/app/components/Header/page.tsx
@@ -3,22 +3,22 @@ import React from 'react';
 
 const Header = () => {
     return ( 
-    <div className="relative flex flex-row gap-2 w-full h-12 text-left text-base text-black font-regular-16-24">
-        <input
-            id='searchbar'
-            placeholder='Search here'
-            type="search"
-            className="relative rounded-xl focus:outline-none bg-white shadow-md w-full h-full flex items-start justify-start p-2 gap-40 text-left text-base font-regular border overflow-hidden border-gray-300"
-        />
+      <div className="relative flex flex-row gap-2 w-full h-12 text-left text-base text-black font-regular-16-24">
+      <input
+          id='searchbar'
+          placeholder='Search here'
+          type="search"
+          className="relative rounded-xl focus:outline-none bg-white shadow-md w-full h-full flex items-start justify-start p-2 gap-40 text-left text-base font-regular border overflow-hidden border-gray-300"
+      />
 
 
-        <div className="flex items-center justify-center w-1/3 h-full rounded-lg bg-gray-200">
-          Bell
-        </div>
-        <div className="flex items-center justify-center w-1/3 h-full rounded-lg bg-gray-200">
-          Avatar
-        </div>
-    </div>
+      <div className="flex items-center justify-center w-1/3 text-white h-full rounded-lg bg-gray-200">
+        Bell
+      </div>
+      <div className="flex items-center justify-center w-1/3 text-white h-full rounded-lg bg-gray-200">
+        Avatar
+      </div>
+  </div>
       
      );
 }
