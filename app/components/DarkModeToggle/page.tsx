03586986a0e61a11cccd82ab377fc9647290b0a9
Abhinav <abhinavtomar251@gmail.com>
2023-12-26 05:40:38 +0530

    dark-mode-button

--- app/components/DarkModeToggle/page.tsx
+++ app/components/DarkModeToggle/page.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import React from 'react';
-
 import { useState, useEffect } from 'react';
 
 const DarkModeToggle = () => {
@@ -11,36 +7,36 @@ const DarkModeToggle = () => {
     const theme = localStorage.getItem('theme');
     if (theme === 'dark') {
       setIsDarkMode(true);
+      document.documentElement.classList.add('dark');
     }
   }, []);
 
   const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  useEffect(() => {
-    if (isDarkMode) {
+    const updatedMode = !isDarkMode;
+    setIsDarkMode(updatedMode);
+    localStorage.setItem('theme', updatedMode ? 'dark' : 'light');
+    if (updatedMode) {
       document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
     } else {
       document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
     }
-  }, [isDarkMode]);
+  };
 
   return (
     <div
-      id='dark-mode-toggle'
-      className={`relative flex border-2 border-solid items-center justify-start w-8 h-3 cursor-pointer rounded-full  bg-white dark:bg-gray-900`}
-      onClick={toggleDarkMode}
-    >
-      <div
-        id='switch'
-        className={`absolute w-3 h-3 bg-black rounded-full shadow-md dark:bg-white transition-transform duration-300 ${
-          isDarkMode ? 'transform translate-x-0' : 'transform translate-x-full'
-        }`}
-      ></div>
-    </div>
+    id='dark-mode-toggle'
+    className={`relative flex border-2 border-solid items-center justify-start w-8 h-3 cursor-pointer rounded-full  bg-white dark:bg-gray-900 transition-colors duration-300 ${
+      isDarkMode ? 'bg-white' : 'bg-gray-900 dark:bg-gray-900'
+    } `}
+    onClick={toggleDarkMode}
+  >
+    <div
+      id='switch'
+      className={`absolute w-3 h-3 bg-black rounded-full shadow-md dark:bg-white transition-transform duration-300 ${
+        isDarkMode ? 'bg-black dark:bg-white transform translate-x-0' : 'bg-white dark:bg-gray-900 transform translate-x-full'
+      }`}
+    ></div>
+  </div>
   );
 };
 
