249539b137738544b17796f20ee38864eb6a238a
Abhinav <abhinavtomar251@gmail.com>
2024-01-22 00:00:06 +0530

    Database Management Page

---
+++ Frontend/app/components/DatabaseManagement/Users.tsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from 'react';
+
+
+interface UserData {
+    fieldName: string;
+    dataType: string;
+  }
+
+
+const Users = () => {
+    const entries: UserData[] = [
+    { fieldName: "Password", dataType: "String" },
+    { fieldName: "Full Name", dataType: "String" },
+    { fieldName: "Uid", dataType: "String" },
+    { fieldName: "Email", dataType: "String" },
+    { fieldName: "Display_Name", dataType: "String" },
+    { fieldName: "Password", dataType: "String" },
+    { fieldName: "Created_time", dataType: "Data_Time" },
+    { fieldName: "Phone_number", dataType: "String" },
+    { fieldName: "Phone_url", dataType: "Img_Path" },
+  ];
+  const [data, setData] = useState(entries);
+  const [newField, setNewField] = useState<UserData>({
+    fieldName: "",
+    dataType: "",
+  });
+  const handleDelete = (index: number) => {
+    setData((prev) => prev.filter((_, idx) => idx !== index));
+  };
+  const handleAdd = () => {
+    console.log(newField);
+    setData((prev) => {
+      return [...prev, newField];
+    });
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return (
+    <div>
+      <div className="border-box max-w-full flex justify-between items-center">
+        <div className="text-[22px] font-[700]">Users</div>
+        <div className="flex min-w-[50%] flex-row justify-end gap-[10px] items-center">
+          <button className="p-2 h-[36px] flex flex-row justify-around rounded-[5px] items-center bg-mediumblue text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M12 6C12 4.114 12 3.172 12.586 2.586C13.172 2 14.114 2 16 2H18C19.886 2 20.828 2 21.414 2.586C22 3.172 22 4.114 22 6V8C22 9.886 22 10.828 21.414 11.414C20.828 12 19.886 12 18 12H16C14.114 12 13.172 12 12.586 11.414C12 10.828 12 9.886 12 8V6Z"
+                fill="white"
+              />
+              <path
+                d="M10.5 7C8.943 7.005 8.117 7.054 7.586 7.586C7 8.172 7 9.114 7 11V13C7 14.437 7 15.327 7.26 15.928C7.33669 16.1093 7.44735 16.2743 7.586 16.414C8.172 17 9.114 17 11 17H13C14.886 17 15.828 17 16.414 16.414C16.946 15.883 16.995 15.057 16.999 13.5H15.911C15.045 13.5 14.251 13.5 13.606 13.413C12.895 13.318 12.143 13.093 11.526 12.475C10.907 11.857 10.682 11.105 10.586 10.395C10.5 9.749 10.5 8.955 10.5 8.09V7Z"
+                fill="white"
+              />
+              <path
+                d="M5.5 12C3.943 12.005 3.117 12.054 2.586 12.586C2 13.172 2 14.114 2 16V18C2 19.886 2 20.828 2.586 21.414C3.172 22 4.114 22 6 22H8C9.886 22 10.828 22 11.414 21.414C11.946 20.883 11.995 20.057 11.999 18.5H10.911C10.045 18.5 9.251 18.5 8.606 18.413C7.895 18.318 7.143 18.093 6.526 17.475C5.907 16.857 5.682 16.105 5.586 15.395C5.5 14.749 5.5 13.955 5.5 13.089V12Z"
+                fill="white"
+              />
+            </svg>
+            <span className="ml-2 text-[14px] font-[500]">Manage content</span>
+          </button>
+          <button className="flex items-center rounded-[5px] justify-center p-2 h-[36px] bg-mediumblue text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+            >
+              <path
+                d="M17.5 9.25C17.5 9.84674 17.2629 10.419 16.841 10.841C16.419 11.2629 15.8467 11.5 15.25 11.5C14.6533 11.5 14.081 11.2629 13.659 10.841C13.2371 10.419 13 9.84674 13 9.25C13 8.65326 13.2371 8.08097 13.659 7.65901C14.081 7.23705 14.6533 7 15.25 7C15.8467 7 16.419 7.23705 16.841 7.65901C17.2629 8.08097 17.5 8.65326 17.5 9.25ZM17.5 15.25C17.5 15.8467 17.2629 16.419 16.841 16.841C16.419 17.2629 15.8467 17.5 15.25 17.5C14.6533 17.5 14.081 17.2629 13.659 16.841C13.2371 16.419 13 15.8467 13 15.25C13 14.6533 13.2371 14.081 13.659 13.659C14.081 13.2371 14.6533 13 15.25 13C15.8467 13 16.419 13.2371 16.841 13.659C17.2629 14.081 17.5 14.6533 17.5 15.25ZM17.5 21.25C17.5 21.8467 17.2629 22.419 16.841 22.841C16.419 23.2629 15.8467 23.5 15.25 23.5C14.6533 23.5 14.081 23.2629 13.659 22.841C13.2371 22.419 13 21.8467 13 21.25C13 20.6533 13.2371 20.081 13.659 19.659C14.081 19.2371 14.6533 19 15.25 19C15.8467 19 16.419 19.2371 16.841 19.659C17.2629 20.081 17.5 20.6533 17.5 21.25Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <span className=" text-slate-500"> Schema</span>
+      <table className="min-w-full p-6 overflow-hidden">
+        <thead className=" border-black dark:border-white  border-b border-solid">
+          <tr className="text-left">
+            <th className="py-2 px-4 text-left text-slate-500 border-black dark:border-white  border-b border-solid ">
+              Field Name
+            </th>
+            <th className="py-2 px-4 text-slate-500 border-black dark:border-white  border-b border-solid ">
+              Data Types
+            </th>
+            <th className="py-2 px-4 text-slate-500 border-black dark:border-white  border-b border-solid ">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((field, index) => (
+            <tr key={index + Date.now()} className="text-left">
+              <td className="py-2 px-4 text-left border-black dark:border-white  border-b border-solid ">
+                {field.fieldName}
+              </td>
+              <td className="py-2 px-4 text-left border-black dark:border-white  border-b border-solid ">
+                {field.dataType}
+              </td>
+              <td className="py-2 px-4 text-left border-black dark:border-white  border-b border-solid ">
+                <span onClick={() => handleDelete(index)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 30 30"
+                    fill="none"
+                  >
+                    <path
+                      d="M23.75 5H19.375L18.125 3.75H11.875L10.625 5H6.25V7.5H23.75M7.5 23.75C7.5 24.413 7.76339 25.0489 8.23223 25.5178C8.70107 25.9866 9.33696 26.25 10 26.25H20C20.663 26.25 21.2989 25.9866 21.7678 25.5178C22.2366 25.0489 22.5 24.413 22.5 23.75V8.75H7.5V23.75Z"
+                      fill="black"
+                    />
+                  </svg>
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex flex-row justify-between items-center px-12 text-[16px] font-[500]">
+        <div className="flex flex-row justify-between items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="17"
+            height="14"
+            viewBox="0 0 17 14"
+            fill="none"
+          >
+            <path
+              d="M0 0.625C0 0.45924 0.0658481 0.300269 0.183058 0.183058C0.300269 0.0658481 0.45924 0 0.625 0H14.375C14.5408 0 14.6997 0.0658481 14.8169 0.183058C14.9342 0.300269 15 0.45924 15 0.625C15 0.79076 14.9342 0.949731 14.8169 1.06694C14.6997 1.18415 14.5408 1.25 14.375 1.25H0.625C0.45924 1.25 0.300269 1.18415 0.183058 1.06694C0.0658481 0.949731 0 0.79076 0 0.625ZM0.625 6.25H14.375C14.5408 6.25 14.6997 6.18415 14.8169 6.06694C14.9342 5.94973 15 5.79076 15 5.625C15 5.45924 14.9342 5.30027 14.8169 5.18306C14.6997 5.06585 14.5408 5 14.375 5H0.625C0.45924 5 0.300269 5.06585 0.183058 5.18306C0.0658481 5.30027 0 5.45924 0 5.625C0 5.79076 0.0658481 5.94973 0.183058 6.06694C0.300269 6.18415 0.45924 6.25 0.625 6.25ZM8.75 10H0.625C0.45924 10 0.300269 10.0658 0.183058 10.1831C0.0658481 10.3003 0 10.4592 0 10.625C0 10.7908 0.0658481 10.9497 0.183058 11.0669C0.300269 11.1842 0.45924 11.25 0.625 11.25H8.75C8.91576 11.25 9.07473 11.1842 9.19194 11.0669C9.30915 10.9497 9.375 10.7908 9.375 10.625C9.375 10.4592 9.30915 10.3003 9.19194 10.1831C9.07473 10.0658 8.91576 10 8.75 10ZM15.625 10H14.375V8.75C14.375 8.58424 14.3092 8.42527 14.1919 8.30806C14.0747 8.19085 13.9158 8.125 13.75 8.125C13.5842 8.125 13.4253 8.19085 13.3081 8.30806C13.1908 8.42527 13.125 8.58424 13.125 8.75V10H11.875C11.7092 10 11.5503 10.0658 11.4331 10.1831C11.3158 10.3003 11.25 10.4592 11.25 10.625C11.25 10.7908 11.3158 10.9497 11.4331 11.0669C11.5503 11.1842 11.7092 11.25 11.875 11.25H13.125V12.5C13.125 12.6658 13.1908 12.8247 13.3081 12.9419C13.4253 13.0592 13.5842 13.125 13.75 13.125C13.9158 13.125 14.0747 13.0592 14.1919 12.9419C14.3092 12.8247 14.375 12.6658 14.375 12.5V11.25H15.625C15.7908 11.25 15.9497 11.1842 16.0669 11.0669C16.1842 10.9497 16.25 10.7908 16.25 10.625C16.25 10.4592 16.1842 10.3003 16.0669 10.1831C15.9497 10.0658 15.7908 10 15.625 10Z"
+              fill="black"
+            />
+          </svg>
+          <input
+            placeholder="New Field name......."
+            value={newField.fieldName}
+            onChange={(e) =>
+              setNewField((prev) => {
+                return { ...prev, fieldName: e.target.value };
+              })
+            }
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Data Type"
+            value={newField.dataType}
+            onChange={(e) =>
+              setNewField((prev) => {
+                return { ...prev, dataType: e.target.value };
+              })
+            }
+          />
+        </div>
+        <div>
+          <input placeholder="Is List" />
+        </div>
+        <div>
+          <span className="cursor-pointer" onClick={() => handleAdd()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+            >
+              <path
+                d="M26.25 8.7498L11.25 23.7498L4.375 16.8748L6.1375 15.1123L11.25 20.2123L24.4875 6.9873L26.25 8.7498Z"
+                fill="#4500D8"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Users;
--- Frontend/app/components/DatabaseManagement/page.tsx
+++ Frontend/app/components/DatabaseManagement/page.tsx
@@ -1,21 +1,155 @@
 'use client';
 
 import React from 'react';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import Sidebar from '../Sidebar/page';
 import Header from '../Header/page';
+import Users from './Users';
+import RestaurantOwner from './RestaurantOwner';
+import Influencerlist from './Influencelist';
+
+
+
+
 
 const DatabaseManagement = () => {
+ const router = useRouter();
+const [activeOption, setActiveOption] = useState('');
+const [page, setPage] = useState<
+    "Users" | "Restaurant Owner" | "Influencers"
+  >("Users");
+
     return ( 
       <div className="flex w-full min-h-screen">
         <div className="w-1/4 min-h-screen flex flex-col justify-between">
           <Sidebar />
         </div>
-        <div className="w-full bg-special-bg min-h-screen px-6 py-6 flex flex-col justify-between">
+        <div className="w-full bg-special-bg min-h-screen px-6 py-6 flex flex-col">
           <div className=""><Header/></div>
           <div className='m-0  py-8 h-7 font-bold'>Database Management- user</div>
-          <div className='relative rounded-[10px] flex flex-col bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full h-[51.13rem]'>
-            
+        <div className='relative rounded-[10px] flex flex-row bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-full h-auto'>
+            <div className="overflow-hidden w-1/4 p-2 h-full flex flex-col border-black">
+              <div className="w-full justify-center flex flex-row gap-2 ">
+                
+                  {/* <!-- Button 1 --> */}
+
+                  <button className="bg-mediumblue hover:bg-blue-700 text-white font-bold py-2 px-4 h-[36px] w-[43px] rounded-[5px] flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M7 9V7H21V9H7ZM7 13V11H21V13H7ZM7 17V15H21V17H7ZM4 9C3.71667 9 3.47933 8.904 3.288 8.712C3.09667 8.52 3.00067 8.28267 3 8C3 7.71667 3.096 7.47933 3.288 7.288C3.48 7.09667 3.71733 7.00067 4 7C4.28333 7 4.521 7.096 4.713 7.288C4.905 7.48 5.00067 7.71733 5 8C5 8.28333 4.904 8.521 4.712 8.713C4.52 8.905 4.28267 9.00067 4 9ZM4 13C3.71667 13 3.47933 12.904 3.288 12.712C3.09667 12.52 3.00067 12.2827 3 12C3 11.7167 3.096 11.4793 3.288 11.288C3.48 11.0967 3.71733 11.0007 4 11C4.28333 11 4.521 11.096 4.713 11.288C4.905 11.48 5.00067 11.7173 5 12C5 12.2833 4.904 12.521 4.712 12.713C4.52 12.905 4.28267 13.0007 4 13ZM4 17C3.71667 17 3.47933 16.904 3.288 16.712C3.09667 16.52 3.00067 16.2827 3 16C3 15.7167 3.096 15.4793 3.288 15.288C3.48 15.0967 3.71733 15.0007 4 15C4.28333 15 4.521 15.096 4.713 15.288C4.905 15.48 5.00067 15.7173 5 16C5 16.2833 4.904 16.521 4.712 16.713C4.52 16.905 4.28267 17.0007 4 17Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </button>
+
+                  {/* <!-- Button 2 --> */}
+
+                  <button className="bg-mediumblue hover:bg-blue-700 text-white font-bold h-[36px] w-[43px] py-2 px-4 rounded-[5px] flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M9.25001 22L8.85001 18.8C8.63335 18.7167 8.42935 18.6167 8.23801 18.5C8.04668 18.3833 7.85901 18.2583 7.67501 18.125L4.70001 19.375L1.95001 14.625L4.52501 12.675C4.50835 12.5583 4.50001 12.446 4.50001 12.338V11.663C4.50001 11.5543 4.50835 11.4417 4.52501 11.325L1.95001 9.375L4.70001 4.625L7.67501 5.875C7.85835 5.74167 8.05001 5.61667 8.25001 5.5C8.45001 5.38333 8.65001 5.28333 8.85001 5.2L9.25001 2H14.75L15.15 5.2C15.3667 5.28333 15.571 5.38333 15.763 5.5C15.955 5.61667 16.1423 5.74167 16.325 5.875L19.3 4.625L22.05 9.375L19.475 11.325C19.4917 11.4417 19.5 11.5543 19.5 11.663V12.337C19.5 12.4457 19.4833 12.5583 19.45 12.675L22.025 14.625L19.275 19.375L16.325 18.125C16.1417 18.2583 15.95 18.3833 15.75 18.5C15.55 18.6167 15.35 18.7167 15.15 18.8L14.75 22H9.25001ZM12.05 15.5C13.0167 15.5 13.8417 15.1583 14.525 14.475C15.2083 13.7917 15.55 12.9667 15.55 12C15.55 11.0333 15.2083 10.2083 14.525 9.525C13.8417 8.84167 13.0167 8.5 12.05 8.5C11.0667 8.5 10.2373 8.84167 9.56201 9.525C8.88668 10.2083 8.54935 11.0333 8.55001 12C8.55001 12.9667 8.88768 13.7917 9.56301 14.475C10.2383 15.1583 11.0673 15.5 12.05 15.5Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </button>
+
+                  {/* <!-- Button 3 --> */}
+
+                  <button className="bg-mediumblue hover:bg-blue-700 text-white h-[36px] w-[87px] font-bold py-2 px-4 rounded-[5px] flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M20 18.6904L12.7 22.7404C12.2 23.0004 11.7 23.0004 11.2 22.7404L4 18.6904L17.05 5.54043L17.4 5.44043C17.7 5.44043 17.87 5.57043 17.9 5.84043L20 18.6904ZM9.35 5.74043L4.8 13.2904L6.7 1.34043C6.73 1.07043 6.9 0.94043 7.2 0.94043C7.4 0.94043 7.53 1.00043 7.6 1.19043L9.75 5.14043L9.35 5.74043ZM13.85 7.00043L4.3 16.5904L11.55 4.29043C11.65 4.09043 11.8 4.00043 12 4.00043C12.2 4.00043 12.33 4.09043 12.4 4.29043L13.85 7.00043Z"
+                        fill="white"
+                      />
+                    </svg>
+                    Firebase
+                  </button>
+
+                  {/* <!-- Button 4 --> */}
+
+                  <button className="bg-mediumblue hover:bg-blue-700 text-white h-[36px] w-[43px] font-bold py-2 px-4 rounded-[5px] flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M18 13H13V18C13 18.55 12.55 19 12 19C11.45 19 11 18.55 11 18V13H6C5.45 13 5 12.55 5 12C5 11.45 5.45 11 6 11H11V6C11 5.45 11.45 5 12 5C12.55 5 13 5.45 13 6V11H18C18.55 11 19 11.45 19 12C19 12.55 18.55 13 18 13Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </button>
+                
+              </div>
+
+              <div className="flex flex-col justify-content items-center">
+                <button
+                  className="w-[157px] h-[36px] my-2 mx-6 flex-shrink-0 rounded-[5px] hover:bg-mediumblue text-black
+              hover:text-white text-5xl font-semibold flex items-center justify-center"
+                  onClick={() => setPage("Users")}
+                >
+                  {" "}
+                  Users
+                </button>
+
+                <button
+                  className="w-[157px] h-[36px] my-2 mx-6 flex-shrink-0 rounded-[5px] hover:bg-mediumblue text-black
+              hover:text-white text-5xl font-semibold  flex items-center justify-center"
+                  onClick={() => setPage("Restaurant Owner")}
+                >
+                  {" "}
+                  Restaurant Owner
+                </button>
+
+                <button
+                  className="w-[157px] h-[36px] my-2 mx-6 flex-shrink-0 rounded-[5px] hover:bg-mediumblue text-black
+              hover:text-white text-5xl font-semibold  flex items-center justify-center"
+                  onClick={() => setPage("Influencers")}
+                >
+                  {" "}
+                  Influencers
+                </button>
+              </div>  
+            </div>
+
+
+            {/* Right Side Part for Users page */}
+
+          <div className="flex w-3/4 border-box justify-center overflow-hidden">
+            <div className="p-2">
+              {((): JSX.Element => {
+                return {
+                  Users: <Users />,
+                  "Restaurant Owner": <RestaurantOwner />,
+                  Influencers: <Influencerlist />,
+                }[page];
+              })()}
+            </div>
           </div>
+
+        </div>
         </div>
       </div>
      );
