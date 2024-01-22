2add555d16b851cfdd7b44d850dd3407c5d33633
Abhinav <abhinavtomar251@gmail.com>
2024-01-23 04:01:56 +0530

    ds

--- Frontend/app/Config/firebase.ts
+++ Frontend/app/Config/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -24,8 +24,8 @@ const auth = getAuth(app);
 
 const db = getFirestore(app);
 
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
-export {auth, db, app, analytics};
+export {auth, db, app};
 
 
